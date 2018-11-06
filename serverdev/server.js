9ade78db32b25d435c73d3e070307bf4ccb04c5d
jingyuanmei <>
2018-11-06 09:17:44 +0800

    修改根目录路径

--- serverdev/server.js
+++ serverdev/server.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var app = express();
 var util = require('util');
-var axios = require('axios');
-var apiRoutes = express.Router();
-app.use('/', apiRoutes);
-//app.use(express.static('public'));
+
+app.use(express.static("../public"));
 
 //static是express内置的一个保存静态文件的方法
 //app.get获取 路径router
 //res.send  res.senFile 输出文件到页面上
 //req请求  res响应输出
-app.get('/index.html',function() {
-	res.sendFile(__dirname + '/' + 'index.html')
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
 })
+
 app.get('/form',function(req, res) {
 	var obj = {
 		'first_name': req.query.first_name,
@@ -23,8 +22,19 @@ app.get('/form',function(req, res) {
 	res.end(util.inspect(obj, {depth: null}))
 })
 
-app.post('/location', (req, res) => {
-	console.log(req, 3333)
+// apiRoutes.use('/location', function (req, res) {
+//   setTimeout(() => {
+// 		res.json({
+// 			success: true,
+// 			location: {
+// 				province: '河南省',
+// 				city: '南阳市'
+// 			}
+// 		})
+// 	}, 1000)
+// })
+
+app.get('/location', (req, res) => {
 	setTimeout(() => {
 		res.json({
 			success: true,
